37eab12b020f2df2e7e6a5370cb03ecbcd1ac0d0
Your Name <you@example.com>
2021-01-17 01:32:16 +0100

    initial commit

---
+++ src/test/java/ma/emsi/devoir/commerce/EcommerceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.emsi.devoir.commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
